48470c64fcec428f2a5f655bfe5ee4e5b0646aa9
Devi Willieam Anggara <deviwillieamunggula@googlemail.com>
2024-09-27 08:38:10 +0800

    update
    ok

--- app.py
+++ app.py
@@ -94,15 +94,14 @@ def main():
             st.warning("Incorrect password. Please try again.")
         with st.popover("✨ Model"):
             model = st.selectbox("Select a model:", [
-                "o1-mini-2024-09-12",
-                "o1-preview",
+                "o1-mini",
                 "gpt-4o-2024-05-13",
 		        "gpt-4o-mini-2024-07-18",
 		        "gpt-4-turbo",
                 "gpt-3.5-turbo-16k",
                 "gpt-4",
                 "gpt-4-32k",
-            ], index=1)
+            ], index=0)
 
         with st.popover("⚙️ Model parameters"):
             model_temp = st.slider("Temperature", min_value=0.0, max_value=2.0, value=0.3, step=0.1)
