47cf521e5f4a6f7a3476cf72a8ba95ac47f13a94
Devi Willieam Anggara <deviwillieamunggula@googlemail.com>
2025-03-02 22:52:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -95,6 +95,7 @@ def main():
         with st.popover("✨ Model"):
             model = st.selectbox("Select a model:", [
                 "gpt-4.5-preview-2025-02-27",
+		    "o1-2024-12-17,
 		    "gpt-4o-2024-05-13",
 		        "gpt-4o-mini-2024-07-18",
 		        "gpt-4-turbo",
